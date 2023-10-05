bb5be259f47da1bb013fb0445869923a0a96792e
Matteo Scuro <matteoscuro@Air-di-Matteo.fritz.box>
2023-10-05 08:04:23 +0200

    AYqiD0E7882CViJqbmYn fixed; gestita con eccezione dedicata; 2min

--- container/impl-base/src/main/java/org/jboss/arquillian/container/impl/client/container/ContainerDeployController.java
+++ container/impl-base/src/main/java/org/jboss/arquillian/container/impl/client/container/ContainerDeployController.java
@@ -108,6 +108,9 @@ public class ContainerDeployController {
             public void perform(Container container, Deployment deployment) throws Exception {
                 if (container.getState().equals(Container.State.STARTED) && deployment.isDeployed()) {
                     event.fire(new UnDeployDeployment(container, deployment));
+                } else {
+                    throw new DeploymentException("Trying to deploy a managed deployment " + deployment.getDescription().getName()
+                    + " to a non-started managed container " + container.getName());
                 }
             }
         });
