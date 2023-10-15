f78216a39e821dc6ca1e04f513ca2071104c68c2
stefaclaudia <stefaclaudia@libero.it>
2023-10-15 23:30:50 +0200

    AYqiDz19882CViJqbmTP fixed; gestita eccezione personalizzata, 5min

--- core/spi/src/main/java/org/jboss/arquillian/core/spi/SecurityActions.java
+++ core/spi/src/main/java/org/jboss/arquillian/core/spi/SecurityActions.java
@@ -143,7 +143,7 @@ final class SecurityActions {
             }
             obj = constructor.newInstance(arguments);
         } catch (Exception e) {
-            throw new RuntimeException("Could not create new instance of " + implClass, e);
+            throw new LoadExcp("Could not create new instance of " + implClass, e);
         }
 
         return obj;
