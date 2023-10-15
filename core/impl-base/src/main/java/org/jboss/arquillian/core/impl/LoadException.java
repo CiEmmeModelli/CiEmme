3999a21f10829da7134a21ad3c5ea8306b9a553e
stefaclaudia <stefaclaudia@libero.it>
2023-10-15 17:51:14 +0200

    AYqiDz6R882CViJqbmUL fixed; gestita eccezione personalizzata; 7min

---
+++ core/impl-base/src/main/java/org/jboss/arquillian/core/impl/LoadException.java
@@ -0,0 +1,9 @@
+package org.jboss.arquillian.core.impl;
+
+public class LoadException extends RuntimeException{
+
+    public LoadException(String string, ClassNotFoundException e2) {
+        super(string,e2);
+    }
+
+}
