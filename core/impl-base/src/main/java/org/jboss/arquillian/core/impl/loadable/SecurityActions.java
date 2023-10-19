41ca8f3a261d20b3e751e12a47d7b5baf5351f3d
Matteo Scuro <matteoscuro@Air-di-Matteo.fritz.box>
2023-10-19 07:28:15 +0200

    AYqiDz5t882CViJqbmT4 risolta con no warning; 0min

--- core/impl-base/src/main/java/org/jboss/arquillian/core/impl/loadable/SecurityActions.java
+++ core/impl-base/src/main/java/org/jboss/arquillian/core/impl/loadable/SecurityActions.java
@@ -328,6 +328,7 @@ final class SecurityActions {
     /**
      * Single instance to get the TCCL
      */
+    @SuppressWarnings("java:S6548")
     private enum GetTcclAction implements PrivilegedAction<ClassLoader>
 
     {
