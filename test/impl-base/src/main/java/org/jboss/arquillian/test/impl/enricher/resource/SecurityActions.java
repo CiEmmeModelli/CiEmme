8a16bfd4bc30f2eea1290797c656a2f77dc241bc
stefaclaudia <stefaclaudia@libero.it>
2023-10-22 11:55:26 +0200

    AYqiD0NS882CViJqbma8 fixed; eliminata variabile locale; 3min

--- test/impl-base/src/main/java/org/jboss/arquillian/test/impl/enricher/resource/SecurityActions.java
+++ test/impl-base/src/main/java/org/jboss/arquillian/test/impl/enricher/resource/SecurityActions.java
@@ -229,7 +229,7 @@ final class SecurityActions {
 
     public static List<Field> getFieldsWithAnnotation(final Class<?> source,
         final Class<? extends Annotation> annotationClass) {
-        List<Field> declaredAccessableFields = AccessController.doPrivileged(new PrivilegedAction<List<Field>>() {
+        return AccessController.doPrivileged(new PrivilegedAction<List<Field>>() {
             public List<Field> run() {
                 List<Field> foundFields = new ArrayList<Field>();
                 Class<?> nextSource = source;
@@ -247,7 +247,6 @@ final class SecurityActions {
                 return foundFields;
             }
         });
-        return declaredAccessableFields;
     }
 
     public static List<Method> getMethodsWithAnnotation(final Class<?> source,
