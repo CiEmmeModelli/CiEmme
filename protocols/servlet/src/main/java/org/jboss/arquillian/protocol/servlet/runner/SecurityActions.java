88f61d64288566725278e6c8ed7a7221863e5e70
Matteo Scuro <matteoscuro@Air-di-Matteo.fritz.box>
2023-10-29 08:54:09 +0100

    AYqiD0SF882CViJqbmcd fixed; cambiato tipo eccezione; 1min

--- protocols/servlet/src/main/java/org/jboss/arquillian/protocol/servlet/runner/SecurityActions.java
+++ protocols/servlet/src/main/java/org/jboss/arquillian/protocol/servlet/runner/SecurityActions.java
@@ -178,7 +178,7 @@ final class SecurityActions {
                 }
                 // Just in case we've really messed up
                 catch (final ClassCastException cce) {
-                    throw new RuntimeException("Obtained unchecked Exception; this code should never be reached", t);
+                    throw new RunCustomException("Obtained unchecked Exception; this code should never be reached", t);
                 }
             }
         }
