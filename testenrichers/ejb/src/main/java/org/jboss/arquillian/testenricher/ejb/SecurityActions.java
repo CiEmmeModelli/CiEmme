d3312baf3c8e4900e856873f2bceba217e6fe496
stefaclaudia <stefaclaudia@libero.it>
2023-10-29 19:45:15 +0100

    AYqiDz9N882CViJqbmVc fixed; gestita eccezione personalizzata; 4min

--- testenrichers/ejb/src/main/java/org/jboss/arquillian/testenricher/ejb/SecurityActions.java
+++ testenrichers/ejb/src/main/java/org/jboss/arquillian/testenricher/ejb/SecurityActions.java
@@ -297,7 +297,7 @@ final class SecurityActions {
                 }
                 // Just in case we've really messed up
                 catch (final ClassCastException cce) {
-                    throw new RuntimeException(OBT_EXP, t);
+                    throw new NameException(OBT_EXP, t);
                 }
             }
         }
