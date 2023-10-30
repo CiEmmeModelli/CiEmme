9e15ca528a0ffd9e6c5a1b7d1f845e95f0df0556
Matteo Scuro <matteoscuro@Air-di-Matteo.fritz.box>
2023-10-30 06:53:47 +0100

    AYqiDz8b882CViJqbmVK fixed; gestita con eccezione custom; 1min

--- testenrichers/cdi/src/main/java/org/jboss/arquillian/testenricher/cdi/SecurityActions.java
+++ testenrichers/cdi/src/main/java/org/jboss/arquillian/testenricher/cdi/SecurityActions.java
@@ -297,11 +297,11 @@ final class SecurityActions {
             } else {
                 // No other checked Exception thrown by System.getProtocolProperty
                 try {
-                    throw (RuntimeException) t;
+                    throw (RunCustomException) t;
                 }
                 // Just in case we've really messed up
                 catch (final ClassCastException cce) {
-                    throw new RuntimeException("Obtained unchecked Exception; this code should never be reached", t);
+                    throw new RunCustomException("Obtained unchecked Exception; this code should never be reached", t);
                 }
             }
         }
